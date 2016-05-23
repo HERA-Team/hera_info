54787561f6f25481cbb16382dabead9f91e3b099
Danny Jacobs <daniel.c.jacobs@asu.edu>
2016-05-23 19:54:19 -0400

    deleted pyc and build

--- build/lib/hera-info/info.py
+++
@@ -1,8 +0,0 @@
-class site_info:
-    def __init__(self):
-        self.latitude = '-30:43:17.5'
-        self.longitude = '21:25:41.9'
-        self.altitude = 1100
-        selt.tz = -2
-        self.volume = 11
-
--- build/lib/hera_info/info.py
+++
@@ -1,8 +0,0 @@
-latitude = '-30:43:17.5'
-latitude_r = -0.53619181
-longitude = '21:25:41.9'
-longitude_r = 5.6099172
-altitude = 1100
-tz = -2
-volume = 11
-
